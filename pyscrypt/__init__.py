25405771cad071721c0e572a5ab43b9e41c28eb4
Richard Moore <me@ricmoo.com>
2014-05-01 22:17:54 -0400

    Updated version to match pypi.

--- pyscrypt/__init__.py
+++ pyscrypt/__init__.py
@@ -34,6 +34,6 @@
 
 from pyscrypt.hash import hash
 
-VERSION = [0, 1]
+VERSION = [1, 0]
 
 __all__ = ['hash']
--- tests/run-tests.py
+++ tests/run-tests.py
@@ -23,6 +23,8 @@
 
 import pyscrypt
 
+print "Version:", ".".join(str(p) for p in pyscrypt.VERSION)
+
 Tests = [
     dict(password = 'password', salt = 'salt', N = 2, r = 1, p = 1, dkLen = 32, result = '6d1bb878eee9ce4a7b77d7a44103574d4cbfe3c15ae3940f0ffe75cd5e1e0afa'),
     dict(password = 'password', salt = 'salt', N = 32, r = 4, p = 15, dkLen = 128, result = '19f255f7dbcc4128e3467c78c795cb934a82bb813793d2634f6e3adbaee1f54b118fca8b067ab4aad3f6557c716b3734bb93a5cb40500b5e42dc96ccee260fc64d8e660b80e7aecd81c83fefedaf1319b6265e6ef37ca268247052f0b5cac91d14800c1b6f8cb23a28f4620aa0a8e12de88906ec5755a4a643917947a010b7bf'),
